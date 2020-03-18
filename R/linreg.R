11ff000c80441f84c21e85d4f32bbc6ed6688ec4
Ricardo Simpao <rlsimpao@ucdavis.edu>
2020-03-17 20:30:58 -0700

    Update linreg.R
    added comments

--- R/linreg.R
+++ R/linreg.R
@@ -3,24 +3,42 @@
 #' Long Description
 #'
 #'
-#' @param x
-#' @param y
+#' @param x matrix
+#' @param y column vector
 #'
-#' @return double
+#' @return lr, linear model
 #' @export
 #' @examples
 #'
 
 linear_reg <- function(x, y) {
+  #observation count
   n <- dim(x)[1]
+  #parameter count
   p <- dim(x)[2] + 1
+
+  #reponse variable
   y <- as.matrix(y)
+
+  #explanatory variables
   x1 <- as.matrix(cbind(Intercept = rep(1, n), x))
+
+  # (X'X)^-1
   xtx_inv <- solve(t(x1) %*% x1)
+
+  #Betas
   coeffs <- xtx_inv %*% t(x1) %*% y
+
+  #fitted values
   fv <- x1 %*% coeffs
+
+  #residuals
   res <- y - fv
+
+  #covariance matrix?
   s2 <- sum(res^2) / (n - p)
+
+  #returns an fitted model object, with features: coeffs, fitted values, residuals,sigma hat2, n, p
   return(list(coefficients = coeffs, fitted_values = fv, residuals = res,
               sigma_2_hat = s2, xtx_inverse = xtx_inv, n = n, p = p))
 }
