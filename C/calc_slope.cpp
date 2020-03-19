b2d8558fd489876b4fce8bf80bf13069ac61fb98
WilliamShih9 <50122387+WilliamShih9@users.noreply.github.com>
2020-03-19 01:52:56 -0700

    Removed extra function

--- C/calc_slope.cpp
+++ C/calc_slope.cpp
@@ -28,26 +28,3 @@ double calc_slopeC(DataFrame df){
   }
   return sumSXY/sumSXX;
 }
-
-// [[Rcpp::export]]
-double calc_slopeC2(DataFrame df){
-  NumericVector x = df["x"];
-  int length = x.size();
-  NumericVector y = df["y"];
-  double meanX = 0;
-  double meanY = 0;
-  for (int i = 0; i < length; i++){
-    meanX += x[i];
-    meanY += y[i];
-  }
-  meanX /= length;
-  meanY /= length;
-  double sumSXX = 0;
-  double sumSXY = 0;
-  for (int i = 0; i < length; i++){
-    double temp = x[i] - meanX;
-    sumSXX += temp*temp;
-    sumSXY += temp*(y[i] - meanY);
-  }
-  return sumSXY/sumSXX;
-}
